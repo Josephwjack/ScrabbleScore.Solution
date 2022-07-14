a14ebacde24d9e548e5e1cb46fd0602304432b33
Josephwjack <joseph.w.jack@gmail.com>
2022-07-14 16:30:21 -0700

    finish adding logic for scrabble score
    Co-authored-by: Filmer Tolentino <filmer.tolentino@gmail.com>
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>
    Co-authored-by: Jonathan Lu <jdlu.coding@gmail.com>

--- ScrabbleScore/Models/Scrabble.cs
+++ ScrabbleScore/Models/Scrabble.cs
@@ -2,34 +2,59 @@ using System;
 using System.Collections.Generic;
 
 
+
 namespace ScrabbleScore.Models
 {
   
   public class Scrabble
   {
+    // properties
     public string Description { get; set; }
-
-    public static Dictionary<string, int> letters = new Dictionary<string, int>() {
-      {"A", 1}, {"E", 1}, {"I", 1}, {"O", 1}, {"U", 1}, {"L", 1}, {"N", 1}, {"R", 1}, {"S", 1}, {"T", 1}, {"D", 2}, {"G", 2}, {"B", 3}, {"C", 3}, {"M", 3}, {"P", 3}, {"F", 4}, {"H", 4}, {"V", 4}, {"W", 4}, {"Y", 4}, {"K", 5}, {"J", 8}, {"X", 8}, {"Q", 10}, {"Z", 10}
-    };
-    
-    // public static Dictionary<char, int> GetAll()
-    // {
-    //   return letters;
-    // }
-
+    public static int Score { get; set; }
+    // consturctor
     public Scrabble(string description)
     {
       Description = description.ToUpper();
-      // Letters = letters;
-      // letters.Add(this);
+      Score = 0;
     }
-    // public static string GetValue()
-    // {
-    //   foreach (KeyValuePair<string, int> element in letters)
-    //   {
-    //     return Console.WriteLine("Key: {0}, Value: {1}", element.Key, element.Value);
-    //   }
-    // }
+
+    public static Dictionary<char, int> letters = new Dictionary<char, int>() {
+      {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'L', 1}, {'N', 1}, {'R', 1}, {'S', 1}, {'T', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3}, {'F', 4}, {'H', 4}, {'V', 4}, {'W', 4}, {'Y', 4}, {'K', 5}, {'J', 8}, {'X', 8}, {'Q', 10}, {'Z', 10}
+    };
+
+    public static void GetValue()
+    {
+      foreach (KeyValuePair<char, int> entry in letters)
+      {
+        Console.WriteLine("Key: {0}, Value: {1}", entry.Key, entry.Value);
+      }
+    }
+
+    public static void IsPalindrome(string userInput)
+    {
+      char[] uInput = userInput.ToCharArray();
+      foreach (char ch in uInput)
+      {
+      // zzzzzz 
+        foreach (KeyValuePair<char, int> entry in letters)
+        {
+      
+          if (ch == entry.Key)
+          {
+            Score += entry.Value;
+            // Console.WriteLine(Score);
+          }
+        }
+      }
+      Console.WriteLine(Score);
+    }
   }
+
 }
+
+
+// take the word and break it into an array
+// foreach each letter and grab the corresponding value from the dictionary
+// add all the values together
+
+// add for loop to GetValue(
--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -2,22 +2,24 @@ using System;
 using System.Collections.Generic;
 using ScrabbleScore.Models;
 
+
 namespace ScrabbleScore
 {
   public class Program
   {
-  public static void Main() 
-  {
-  Console.WriteLine("Type in a word");
-  string description = Console.ReadLine();
-  Scrabble newScrabble = new Scrabble(description);
-  string result = newScrabble.Description;
-  
-  Console.WriteLine(result);
-  // Scrabble.GetValue();
-  Console.WriteLine(Scrabble.letters["G"]);
-
-  
-  }
+    public static void Main() 
+    {
+    Console.WriteLine("Type in a word");
+    string description = Console.ReadLine();
+    Scrabble newScrabble = new Scrabble(description);
+    string result = newScrabble.Description;
+    // char[] uInput = userInput.ToCharArray();
+    // Console.WriteLine(uInput);
+    // Console.WriteLine(Scrabble.letters["G"]);
+    // Scrabble.GetValue();
+    Scrabble.IsPalindrome(result);
+//Pneumonoultramicroscopicsilicovolcanoconiosis
+    
+    }
   }
 }
